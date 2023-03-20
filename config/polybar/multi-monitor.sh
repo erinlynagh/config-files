891d4404f74c130897af2cfe164f0ec1ecb80eac
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-03-20 02:31:02 -0400

    polybar update

--- config/polybar/multi-monitor.sh
+++ config/polybar/multi-monitor.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Terminate already running bars
+killall -q polybar
+flag=true
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
+    if $flag; then
+        MONITOR=$m TRAYPOS="right" polybar --reload example &
+        flag=false
+    else
+        MONITOR=$m TRAYPOS="" polybar --reload example &
+    fi
 done
