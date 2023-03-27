59d954e12ce1aceb96e20d0ae45c1fc9ecd3ccbc
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-03-27 06:35:22 -0400

    randomize wallpapers

---
+++ config/fish/functions/randomize-wallpapers.fish
@@ -0,0 +1,4 @@
+function randomize-wallpapers --wraps='feh --bg-fill --randomize /media/Pictures/Wallpapers/*' --description 'alias randomize-wallpapers=feh --bg-fill --randomize /media/Pictures/Wallpapers/*'
+  feh --bg-fill --randomize /media/Pictures/Wallpapers/* $argv
+        
+end
