1add871ace48472d4c4007109e6b4ad3d58d742e
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-03-27 06:58:19 -0400

    rm extra function

--- config/fish/functions/dcss=bash.fish
+++
@@ -1,4 +0,0 @@
-function dcss=bash --wraps=/media/git/personal/dcss_morgues/cp_morgues.sh --description 'alias dcss=bash /media/git/personal/dcss_morgues/cp_morgues.sh'
-  /media/git/personal/dcss_morgues/cp_morgues.sh $argv
-        
-end
