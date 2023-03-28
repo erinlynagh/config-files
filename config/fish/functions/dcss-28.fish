b1b21ee3b4d254cd39ec36ce96c36a1e0c0fd33b
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-03-28 04:55:11 -0400

    adding old version

---
+++ config/fish/functions/dcss-28.fish
@@ -0,0 +1,4 @@
+function dcss-28 --wraps=/media/Games/stone_soup-0.28.0/source/crawl --description 'alias dcss-28=/media/Games/stone_soup-0.28.0/source/crawl'
+  /media/Games/stone_soup-0.28.0/source/crawl $argv
+        
+end
