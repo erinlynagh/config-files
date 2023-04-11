f7dd1822587de6c97f3de7ea5ec47e032eb64bd3
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-04-11 16:58:41 -0400

    using the bin instead of fish alias

--- config/fish/functions/dcss.fish
+++
@@ -1,4 +0,0 @@
-function dcss --wraps=gtg --wraps='bash /media/git/personal/dcss_morgues/cp_morgues.sh&&crawl-tiles' --wraps='bash /media/git/personal/dcss_morgue/cp_morgues.sh&&crawl-tiles' --wraps='~/dcss.sh' --description 'alias dcss=~/dcss.sh'
-  ~/dcss.sh $argv
-        
-end
