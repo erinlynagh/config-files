38734f5996668cb4c31f9324d0ae2c4927f12dd3
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-04-11 15:34:37 -0400

    fixed this file

--- dcss-config/dcss.sh
+++ dcss-config/dcss.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
-cp ~/.crawl/morgue/*.lst /media/git/personal/dcss_morgue/
-cp -f ~/.crawl/macro.txt /media/git/config-files/dcss-config/macro.txt
-cp -f ~/.crawl/init.txt /media/git/config-files/dcss-config/init.txt
+crawlGameDir=/media/Games/crawl/
+crawlBuildDir=/media/Games/crawl/crawl-ref/source/
+crawlMorgueDir=/media/git/personal/dcss_morgue/
+crawlSettingsDir=/media/git/config-files/dcss-config/
+crawlConsole=/media/Games/crawl/crawl-console
+
+
+# u: tiles update (for trunk)
+# cu: console update (for trunk)
+# c: run game in console
+# e: edit the init file and save
+# t: play trunk
+# other: play main branch
 
 if [[ $1 == "-u"  ]]; then
-    cd /media/Games/crawl/crawl-ref/source
+    cd $crawlBuildDir
     git pull
     make TILES=y
+elif [[ $1 == "-cu"  ]]; then
+    cd $crawlBuildDir
+    git pull
+    mv ./crawl ./crawl-tiles
+    make
+    mv ./crawl ./crawl-console
+    rm -f /media/Games/crawl/crawl-console
+    mv ./crawl-console $crawlGameDir
+    mv ./crawl-tiles ./crawl
+elif [[ $1 == "-c" ]]; then
+    cd $crawlGameDir
+    alacritty -e $crawlConsole
+    notify-send "reminder" "backup your morgue files to git occasionally"
 elif [[ $1 == "-e" ]]; then
     micro ~/.crawl/init.txt
-    cp -f ~/.crawl/init.txt /media/git/config-files/dcss-config/init.txt
 elif [[ $1 == "-t" ]]; then
-    cd /media/Games/crawl/
-    exec /media/Games/crawl/crawl-ref/source/crawl
+    cd $crawlGameDir
+    /media/Games/crawl/crawl-ref/source/crawl
+    notify-send "reminder" "backup your morgue files to git occasionally"
 else
 	crawl-tiles
+	notify-send "reminder" "backup your morgue files to git occasionally"
 fi
 
-notify-send "reminder" "backup your morgue files to git occasionally"
+#backup files
+cp ~/.crawl/morgue/*.lst $crawlMorgueDir
+cp -f ~/.crawl/macro.txt $crawlSettingsDir
+cp -f ~/.crawl/init.txt $crawlSettingsDir
