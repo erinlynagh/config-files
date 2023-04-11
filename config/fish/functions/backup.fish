f8c988d91db8643a5de9e2b0921bcfbab3c32678
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-04-11 15:44:32 -0400

    minor fix on return dir

--- config/fish/functions/backup.fish
+++ config/fish/functions/backup.fish
@@ -1,4 +1,4 @@
-function backup --wraps='gtg&&./config-files/scripts/auto-backup-files.sh&&lg' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh&&lg' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh' --description 'alias backup=gtg&&cd config-files&&./scripts/auto-backup-files.sh'
-gtg&&cd config-files&&./scripts/auto-backup-files.sh $argv
-
+function backup --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh&&cd' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd -' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd --' --wraps='gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd ~' --description 'alias backup=gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd ~'
+  gtg&&cd config-files&&./scripts/auto-backup-files.sh -r&&cd ~ $argv
+        
 end
