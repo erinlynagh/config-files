ef622a10a87eb54ca47f0b133e33a29e5e46bb25
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-04-14 15:28:06 -0400

    new upower

---
+++ config/fish/functions/battery-level.fish
@@ -0,0 +1,4 @@
+function battery-level --wraps='upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep time' --description 'alias battery-level=upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep time'
+  upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep time $argv
+        
+end
