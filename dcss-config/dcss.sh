f45d5c0bf2aae111e51400e7d9fc90aaf30613d8
Erin Lynagh <44043948+erinlynagh@users.noreply.github.com>
2023-04-27 13:16:32 -0400

    update for dcss

--- dcss-config/dcss.sh
+++ dcss-config/dcss.sh
@@ -14,11 +14,11 @@ crawlSettingsDir=/media/git/config-files/dcss-config/
 # other: play main branch
 
 if [[ $1 == "-u"  ]]; then
-    cd $crawlBuildDir
+    cd $crawlBuildDir || exit
     git pull
     make TILES=y
-elif [[ $1 == "-cu"  ]]; then
-    cd $crawlBuildDir
+    elif [[ $1 == "-cu"  ]]; then
+    cd $crawlBuildDir || exit
     git pull
     mv ./crawl ./crawl-tiles
     make
@@ -26,22 +26,40 @@ elif [[ $1 == "-cu"  ]]; then
     rm -f /media/Games/crawl/crawl-console
     mv ./crawl-console $crawlGameDir
     mv ./crawl-tiles ./crawl
-elif [[ $1 == "-c" ]]; then
-    cd $crawlGameDir
+    elif [[ $1 == "-c" ]]; then
+    cd $crawlGameDir || exit
     ./crawl-console
-    notify-send "reminder" "backup your morgue files to git occasionally"
-elif [[ $1 == "-e" ]]; then
-    micro ~/.crawl/init.txt
-elif [[ $1 == "-t" ]]; then
-    cd $crawlGameDir
-    /media/Games/crawl/crawl-ref/source/crawl
-    notify-send "reminder" "backup your morgue files to git occasionally"
+    elif [[ $1 == "-cw" ]]; then
+    cd $crawlGameDir || exit
+    kitty --detach -e ./crawl-console
+    elif [[ $1 == "-e" ]]; then
+    code ~/.crawl/init.txt
+    elif [[ $1 == "-t" ]]; then
+    cd $crawlGameDir || exit
+    /media/Games/crawl/crawl-ref/source/crawl &
+    elif [[ $1 == "-h" ]]; then
+    echo "h: show this message"
+    echo "u: tiles update (for trunk)"
+    echo "cu: console update (for trunk)"
+    echo "c: run game in console"
+    echo "e: edit the init file and save"
+    echo "t: play trunk"
+    echo "other: play main branch"
+    elif [[ $1 == "-s" ]]; then
+    cp -f ~/.crawl/morgue/*.txt $crawlMorgueDir
+    cp -f ~/.crawl/morgue/*.lst $crawlMorgueDir
+    cp -f /media/Games/crawl/morgue/*.txt $crawlMorgueDir
+    cp -f /media/Games/crawl/morgue/*.lst $crawlMorgueDir
+    python /media/git/config-files/dcss-config/dcssStats.py
 else
-	crawl-tiles
-	notify-send "reminder" "backup your morgue files to git occasionally"
+    crawl-tiles &
+    notify-send "reminder" "backup your morgue files to git occasionally"
 fi
 
 #backup files
+cp /media/Games/crawl/morgue/*.txt $crawlMorgueDir
+cp /media/Games/crawl/morgue/*.lst $crawlMorgueDir
+cp ~/.crawl/morgue/*.txt $crawlMorgueDir
 cp ~/.crawl/morgue/*.lst $crawlMorgueDir
 cp -f ~/.crawl/macro.txt $crawlSettingsDir
 cp -f ~/.crawl/init.txt $crawlSettingsDir
--- dcss-config/dcssStats.py
+++ dcss-config/dcssStats.py
@@ -1,76 +1,100 @@
 from os import listdir, chdir
+from os import remove as rm
 from os.path import isfile, join
 
-
-def dataSortKey(e):
+def scoreSort(e):
     return e['score']
 
+def seedSort(e):
+    return e['seed']
 
-mypath = "/media/git/personal/dcss_morgue"
-chdir(mypath)
+def updateDictionary(dictionary, line, matchString, extractFunction, dictKey, flag=[False]):
+    if (matchString in line and not flag[0]):
+        flag[0] = True
+        dictionary.update({dictKey: extractFunction(line)})
 
-onlyfiles = [f for f in listdir(
-    mypath) if isfile(join(mypath, f)) and f.endswith(".txt") and f.startswith("morgue-")]
+def FixBrokenGames(data):
+    testedSeeds = []
+    brokenSeeds = []
 
-data = []
-brokenFiles = []
-
-for filename in onlyfiles:
-    fileValues = {}
-    with open(filename) as f:
-        content = f.read().splitlines()
-        scoreAdded = False
-        timeAdded = False
-        speciesAdded = False
-        godAdded = False
-        won = False
-        for line in content:
-            if (line.startswith("Game seed:")):
-                seedValue = line.split(":")[1].strip()
-                fileValues.update({'seed': int(seedValue)})
-            if ("level" in line and not scoreAdded):
-                scoreAdded = True
-                scoreValue = line.split(" ")[0]
-                nameValue = line.split(" ")[1]
-                fileValues.update({'score': int(scoreValue)})
-                fileValues.update({'name': nameValue})
-            if ("The game lasted" in line and not timeAdded):
-                timeAdded = True
-                timeValue = line.strip().split(" ")[3]
-                fileValues.update({'time': timeValue})
-            if ("Began as a" in line and not speciesAdded):
-                speciesAdded = True
-                speciesValue = line.strip().split(" ")[3]
-                backgroundValue = line.strip().split(" ")[4]
-                fileValues.update({'species': speciesValue})
-                fileValues.update({'background': backgroundValue})
-            if ("You worshipped" in line and not godAdded):
-                godAdded = True
-                godValue = line.strip().split(" ")[2].split(".")[0]
-                fileValues.update({'god': godValue})
-            if ("Escaped with the Orb" in line):
-                won = True
-                fileValues.update({'won': True})
-        if ('seed' not in fileValues):
-            brokenFiles.append(filename)
+    for x in data:
+        if(x['seed'] in testedSeeds and x['seed'] not in brokenSeeds):
+            brokenSeeds.append(x['seed'])
         else:
-            if (not godAdded):
-                fileValues.update({'god': "Atheist"})
-            if (not won):
-                fileValues.update({'won': False})
-            fileValues.update({'filename': filename})
-            data.append(fileValues)
+            testedSeeds.append(x['seed'])
 
-data.sort(key=dataSortKey)
+    for x in [x for x in data if x['seed'] in brokenSeeds]:
+        rm(x['filename'])
+        data.remove(x)
 
-totalTime = 0
+def PrintStats(data):
+    totalTime = 0
+    for entry in data:
+        for t in entry['time'].split(":"):
+            totalTime += int(t)*pow(60, 2-entry['time'].split(":").index(t))
 
-for entry in data:
-    for t in entry['time'].split(":"):
-        totalTime += int(t)*pow(60, 2-entry['time'].split(":").index(t))
+    print('Wins:')
+    for x in data:
+        if x['won']:
+            print(x['date'])
+            print("Level " + str(x['level']) + " " +x["species"] + " " + x["background"] + " of " + x["god"] + " (" + str(x['runes']) + " runes)\n\tScored " +
+                str(x["score"]) + " in " + x["time"] + " on " + x["date"])
+        
+    print()
+    print("Stats:")
+    print("Total time played: " + str(round((totalTime/60/60),2)) + " hours")
+    print("Total games played: " + str(len(data)))
+    print("Total wins: " + str(len([x for x in data if x['won']])))
+    print("High Score: " + str(data[0]['score']))
+    print("Win Percentage: " +
+        str(round(len([x for x in data if x['won']])/len(data)*100,2)) + "%")
 
-print("Total time played: " + str(totalTime/60/60) + " hours")
-print("Total games played: " + str(len(data)))
-print("Total wins: " + str(len([x for x in data if x['won']])))
-print("High Score: " + str(data[-1]))
-print("Wins data: " + str([x for x in data if x['won']]))
+morguePath = "/media/git/personal/dcss_morgue"
+MatchStrings = ["Game seed:", "level", "The game lasted", "Began as", "Began as", "Began as", "You worshipped", "Escaped with the Orb", "}:", "XL"]
+# Basically the transform function applied to the line to get the data we want
+LambdaFunctions = [lambda x: int(x.split(":")[1].strip()), #seed
+                    lambda x: int(x.split(" ")[0]), #score
+                    lambda x: x.strip().split(" ")[3], #time
+                    lambda x: x.strip().split(" ")[3], #species
+                    lambda x: x.strip().split(" ")[4], #background
+                    lambda x: x.strip().split("on")[-1].strip()[:-1], #date
+                    lambda x: x.strip().split(" ")[2].split(".")[0], #god
+                    lambda x: True, #won
+                    lambda x: int(x.strip().split(" ")[1].split("/")[0]), #runes
+                    lambda x: int(x.strip().split(":")[4].strip().split(" ")[0]) #level
+                ]
+dictKeys = ['seed', 'score', 'time', 'species', 'background', 'date', 'god', 'won', 'runes', 'level']
+
+def main():
+    data = []
+    morgueFiles = [f for f in listdir(morguePath) if isfile(join(morguePath, f)) and f.startswith("morgue-") and f.endswith(".txt")]
+    chdir(morguePath)
+
+    for filename in morgueFiles:
+        fileValues = {}
+        with open(filename) as f:
+            content = f.read().splitlines()
+            flags = []
+            for i in range(len(MatchStrings)):
+                flags.append([False])
+            for line in content:
+                for i in range(len(MatchStrings)):
+                    updateDictionary(fileValues, line, MatchStrings[i], LambdaFunctions[i], dictKeys[i], flags[i])
+            if (dictKeys[0] not in fileValues):
+                continue
+            else:
+                if dictKeys[6] not in fileValues:
+                    fileValues.update({dictKeys[6]: "Atheist"})
+                if dictKeys[7] not in fileValues:
+                    fileValues.update({dictKeys[7]: False})
+                    fileValues.update({dictKeys[8]: 0})
+                fileValues.update({'filename': filename})
+                data.append(fileValues)
+    data.sort(key=scoreSort, reverse=True)
+    FixBrokenGames(data)
+    PrintStats(data)
+
+
+
+if __name__ == "__main__":
+    main()
