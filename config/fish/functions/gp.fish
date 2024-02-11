a52365258a723caf34249edb150d7835e510d24f
Erin Lynagh <lynaghe@gmail.com>
2024-02-11 06:54:39 -0600

    clear selection after copy

--- config/fish/functions/gp.fish
+++ config/fish/functions/gp.fish
@@ -1,4 +1,4 @@
-function gp --wraps='rbw get github Token&&git push' --wraps='rbw get github Token | xclip -selection c&&git push' --wraps=echo\ \'password\ copied\ to\ clipboard\'\&\&rbw\ get\ github\ Token\ \|\ xclip\ -selection\ c\&\&git\ push --wraps=sudo\ echo\ \'password\ copied\ to\ clipboard\'\&\&rbw\ get\ github\ Token\ \|\ xclip\ -selection\ c\&\&git\ push --wraps='git push' --description 'alias gp=rbw get github Token | xclip -selection c&&git push'
-  rbw get github Token | xclip -selection c&&git push $argv
+function gp --wraps='rbw get github Token&&git push' --wraps='rbw get github Token | xclip -selection c&&git push' --wraps=echo\ \'password\ copied\ to\ clipboard\'\&\&rbw\ get\ github\ Token\ \|\ xclip\ -selection\ c\&\&git\ push --wraps=sudo\ echo\ \'password\ copied\ to\ clipboard\'\&\&rbw\ get\ github\ Token\ \|\ xclip\ -selection\ c\&\&git\ push --wraps='git push' --wraps='rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c' --description 'alias gp=rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c'
+  rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c $argv
         
 end
