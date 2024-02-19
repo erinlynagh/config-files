4fd40959a85327f5ef9b11a93f9ee6f45f9b46bd
Erin Lynagh <lynaghe@gmail.com>
2024-02-18 18:44:34 -0600

    updated script to change inputs

--- config/toggle-speaker.sh
+++ config/toggle-speaker.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # get default sink, using pacmd info, then grepping the default sink name, and finally using awk to get the sink number based on that name
+pactl list short sinks
 defualt_sink=$(pactl list short sinks | grep -e $(pacmd info | grep -e "Default sink name" | awk '{print $4}') | awk '{print $1}')
+echo default sink: $defualt_sink
 # get all sinks
 sinks=($(pactl list short sinks | awk '{print $1}'))
-#remove the default sink from the list
-unset sinks[$defualt_sink]
+
+# loop through all sinks and set the other sink as the default sink
+for sink in ${sinks[@]}; do
+    if [[ $sink != $defualt_sink ]]; then
+        pactl set-default-sink $sink
+        break
+    fi
+done
+
 # set the default sink to the first sink in the list (the other sink)
-pactl set-default-sink ${sinks[@]}
 echo Now using: $(pacmd info | grep -e "Default sink name" | awk '{print $4}')
