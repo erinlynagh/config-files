44958984d8d89c376371a95b60d9d719f59c4471
erin <lynaghe@gmail.com>
2024-02-24 11:25:32 -0600

    created exclude for backup

---
+++ scripts/excludeFiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# variables
+mountPoint=/mnt/media
+backupDirectory=$mountPoint/git/config-files
+cd $backupDirectory/scripts
+
+input=$backupDirectory/scripts/backupSettings/excludeFiles.txt
+while IFS= read -r line; do
+    if [ "$line" == "/files to exclude" ] || [ -z "$line" ] || [[ $line == "end" ]]; then
+        continue
+    fi
+
+    sudo rm -rfv $backupDirectory${line}
+done <"$input"
