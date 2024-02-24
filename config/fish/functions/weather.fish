ce441f8265ded7ad6d1374e1f0b19ecd9398ee0b
erin <lynaghe@gmail.com>
2024-02-24 00:29:45 -0600

    new weather app

--- config/fish/functions/weather.fish
+++ config/fish/functions/weather.fish
@@ -1,4 +1,4 @@
-function weather --wraps='curl wttr.in' --description 'alias weather=curl wttr.in'
-  curl wttr.in $argv
+function weather --wraps='curl wttr.in/?m' --wraps=curl\ wttr.in\\\?m --description alias\ weather=curl\ wttr.in\\\?m
+  curl wttr.in\?m $argv
         
 end
