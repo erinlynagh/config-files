670fb2c0fc349f2cd639ccb2a6d4fda7f1ce170d
erin <lynaghe@gmail.com>
2024-02-28 11:43:07 -0600

    themes for oh-my-posh

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+### Themes
+## Minimal Themes
+## one line; no git
+# oh-my-posh init fish --config ~/.config/oh-my-posh/themes/space.omp.json | source
+## very minimal; has git
+# oh-my-posh init fish --config ~/.config/oh-my-posh/themes/pure.omp.json | source
+
+## Colourful Themes
+# oh-my-posh init fish --config ~/.config/oh-my-posh/themes/M365Princess.omp.json | source
 oh-my-posh init fish --config ~/.config/oh-my-posh/themes/dracula.omp.json | source
+
+## Other Apps
 zoxide init fish | source
 cod init $fish_pid fish | source
