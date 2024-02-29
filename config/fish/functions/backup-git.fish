7886362ebab4f8cf6347577bcf6df408b0ff0ac5
erin <lynaghe@gmail.com>
2024-02-28 22:01:56 -0600

    pushes now

--- config/fish/functions/backup-git.fish
+++ config/fish/functions/backup-git.fish
@@ -1,4 +1,4 @@
-function backup-git --wraps='gtg&&z config-files&&backup&&lg' --wraps='gtg&&cd config-files&&backup&&lg' --description 'alias backup-git=gtg&&cd config-files&&backup&&lg'
-  gtg&&cd config-files&&backup&&lg $argv
+function backup-git --wraps='gtg&&z config-files&&backup&&lg' --wraps='gtg&&cd config-files&&backup&&lg' --wraps='gtg&&cd config-files&&backup&&lg&&gp' --description 'alias backup-git=gtg&&cd config-files&&backup&&lg&&gp'
+  gtg&&cd config-files&&backup&&lg&&gp $argv
         
 end
