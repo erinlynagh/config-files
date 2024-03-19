9b007254fa3b52f208e539a381a02361c0f34e65
erin <lynaghe@gmail.com>
2024-03-18 22:29:06 -0400

    updates

--- config/fish/functions/tarz.fish
+++ config/fish/functions/tarz.fish
@@ -1,4 +1,4 @@
-function tarz --wraps='tar --use-compress-program="pigz -k" -hcvf' --description 'alias tarz=tar --use-compress-program="pigz -k" -hcvf'
-  tar --use-compress-program="pigz -k" -hcvf $argv
+function tarz --wraps='tar --use-compress-program="pigz -k" -hcvf' --wraps='tar --use-compress-program "pigz -1 -k" -hcvf ' --description 'alias tarz=tar --use-compress-program "pigz -1 -k" -hcvf '
+  tar --use-compress-program "pigz -1 -k" -hcvf  $argv
         
 end
