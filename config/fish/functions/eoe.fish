ac4224dfe232b4a8f47951c5f6040d2523a6427b
Erin Lynagh <lynaghe@gmail.com>
2024-06-17 00:58:58 -0400

    updates

---
+++ config/fish/functions/eoe.fish
@@ -0,0 +1,4 @@
+function eoe --wraps=/mnt/media/Games/end_of_eden_term --wraps='cd /mnt/media/Games&&end_of_eden_term&& cd -' --wraps='cd /mnt/media/Games&&./end_of_eden_term&& cd -' --description 'alias eoe=cd /mnt/media/Games&&./end_of_eden_term&& cd -'
+  cd /mnt/media/Games&&./end_of_eden_term&& cd - $argv
+        
+end
