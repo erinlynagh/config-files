e3f3ad5b8e5bbe463cc616f62b670805db9ca595
Erin <lynaghe@gmail.com>
2024-10-04 23:53:44 -0500

    new settings

--- config/fish/functions/gp.fish
+++ config/fish/functions/gp.fish
@@ -1,4 +1,4 @@
-function gp --wraps='rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c' --description 'alias gp=rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c'
-  rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c $argv
+function gp --wraps='copyq disable&&rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c&&copyq enable' --description 'alias gp=copyq disable&&rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c&&copyq enable'
+  copyq disable&&rbw get github Token | xclip -selection c&&git push&&echo -n | xclip -selection c&&copyq enable $argv
         
 end
