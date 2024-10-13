f80a633ca2b09a47576f775bb4dd03dc647ee798
Erin <lynaghe@gmail.com>
2024-10-13 02:21:54 -0500

    remove old wraps

--- config/fish/functions/d.fish
+++ config/fish/functions/d.fish
@@ -1,4 +1,4 @@
-function d --wraps='cd /mnt/media' --wraps='cd /run/media/erin/media/' --description 'alias d=cd /mnt/media'
+function d --wraps='cd /mnt/media' --wraps='cd /mnt/media/' --description 'alias d=cd /mnt/media'
   cd /mnt/media $argv
         
 end
