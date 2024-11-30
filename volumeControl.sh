7bfc067d85c272ef1d588f1203d6cad504e0a568
Erin <lynaghe@gmail.com>
2024-11-30 04:45:29 -0600

    new volume controles

---
+++ volumeControl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# if the first argument is -i or -d, then increase or decrease the volume by the second argument (or 5% by default) with pamixer
+# otherwise, run pamixer -t to toggle the volume
+
+# then, use notify-send to show the new volume level
+
+if [ "$1" == "-i" ] || [ "$1" == "-d" ]; then
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        pamixer -u
+        exit
+    fi
+    if [ "$2" -eq "$2" ] 2>/dev/null; then
+        :
+    else
+        echo "Usage: volumeControl.sh [-i|-d] [amount]"
+        exit 1
+    fi
+fi
+
+if [ "$1" == "-i" ]; then
+    pamixer -i ${2:-5}
+elif [ "$1" == "-d" ]; then
+    pamixer -d ${2:-5}
+elif [ "$1" == "-m" ]; then
+    pamixer --mute
+else
+    pamixer -t
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        # notify-send "Volume: muted"
+        exit
+    else
+        # notify-send "Volume: $(pamixer --get-volume)%"
+        exit
+    fi
+fi
+
+# notify-send "Volume: $(pamixer --get-volume)%"
