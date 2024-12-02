4aefe1d79ca91bb859b4bc5d8f02fb5fc02436c6
Erin <lynaghe@gmail.com>
2024-12-02 04:48:44 -0600

    new greeting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,3 +13,13 @@ end
 # oh-my-posh init fish --config ~/.config/oh-my-posh/themes/M365Princess.omp.json | source
 oh-my-posh init fish --config ~/.config/oh-my-posh/themes/dracula.omp.json | source
 
+set best_flag "\033[48;5;196m\033[38;5;6m██\033[0m\033[48;5;196m\033[38;5;5m██\033[0m\033[48;5;196m\033[38;5;15m██\033[0m\033[48;5;196m\033[38;5;5m██\033[0m\033[48;5;196m\033[38;5;6m██\033[0m\n"
+
+for x in (seq 5)
+    if test "$x" -eq 3
+        echo " :3 :3 :3"
+    else
+        printf  $best_flag
+    end
+end
+echo
